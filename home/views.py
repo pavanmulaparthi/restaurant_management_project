a3a08907b1a91940e80a2856f4ff52c08edcb160
pavanmulaparthi <105601103+pavanmulaparthi@users.noreply.github.com>
2025-08-25 15:55:23 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  context={{
+    'Restaurant_Name':'Vanilla_Bytes'
+    'welcome_message':'We are delighted to serve you the finest Italian cuisine'
+  }
+         }
+  return render(request,'home.html',context)

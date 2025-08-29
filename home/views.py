907a153d315152f6e78b77a6dfa5c359150653a8
pavanmulaparthi <105601103+pavanmulaparthi@users.noreply.github.com>
2025-08-29 11:04:55 +0530

    37209

--- home/views.py
+++ home/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-  context={{
-    'Restaurant_Name':'Vanilla_Bytes'
-    'welcome_message':'We are delighted to serve you the finest Italian cuisine'
-  }}
-  info = Restaurant.objects.first()
-  return render(request,'home.html',context,{restaurant:info})
-
